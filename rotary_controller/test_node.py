98940a6934fde433ac92fc9a53bf0f47182cd8a6
brain0071 <18341314091brain@gmail.com>
2025-07-15 21:31:24 +0800

    0715

--- rotary_controller/test_node.py
+++ rotary_controller/test_node.py
@@ -4,6 +4,7 @@ from std_srvs.srv import SetBool
 from mavros_msgs.msg import ActuatorControl
 # gripper and light
 from mavros_msgs.msg import Altitude
+from std_msgs.msg import Bool, Float32
 
 
 class TEST_Wrapper(Node):
@@ -14,29 +15,48 @@ class TEST_Wrapper(Node):
         self.controller_state = True
         self.motor_pub = self.create_publisher(ActuatorControl, "/mavros/actuator_control", 10)
         self.gripper_light_pub = self.create_publisher(Altitude, "/mavros/gripper_light_control", 10)
-        self.control_freq = 200
+        
+        self.gripper_value = 1100.0
+        self.light_value = 1100.0
+
+        self.create_subscription(Bool, '/light', self.light_callback, 10)
+        self.create_subscription(Float32, '/gripper', self.gripper_callback, 10)
+        
+        self.control_freq = 20
         self.dt = 1 / self.control_freq
-        self.timer = self.create_timer(self.dt, self.callback)
+        self.timer = self.create_timer(self.dt, self.control_callback)
         self.create_service(SetBool, "stop_signal", self.stop_callback)
         
-    def callback(self):
+
+    
+    def light_callback(self, msg):
+        
+        if msg.data == True:
+            self.light_value = 1900.0
+        else:
+            self.light_value = 1100.0
+
+    
+    def gripper_callback(self, msg):
+        
+        self.gripper_value = msg.data
+
+
+
+    def control_callback(self):
         
         if self.controller_state == True:
             motor = ActuatorControl()
-            motor.controls = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]
+            motor.controls = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0]
             self.motor_pub.publish(motor)
 
             gripper_light = Altitude()
             # gripper
-            gripper_light.local = 1900.0
+            gripper_light.local = self.gripper_value
             # light
-            gripper_light.relative = 1900.0            
+            gripper_light.relative = self.light_value            
             self.gripper_light_pub.publish(gripper_light)
 
-            # gripper = Thrust()
-            # gripper.thrust = 1900
-            # self.gripper_pub.publish(gripper)
-            
         else:
             pass
 
